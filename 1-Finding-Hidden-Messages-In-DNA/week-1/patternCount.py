92af1b66b2662aade087ca67315960ad9755ad91
MahaAmin <mahaamin97@gmail.com>
2021-07-22 08:18:43 +0200

    week-1: patternCount(text,pattern)

---
+++ 1-Finding-Hidden-Messages-In-DNA/week-1/patternCount.py
@@ -0,0 +1,22 @@
+def readInput(fileName):
+    # TO-DO: validate input
+    f = open(fileName, 'r')
+    lines = f.readlines()
+    l = len(lines)
+    for i in range(l):
+        lines[i] = lines[i].rstrip()
+    return lines[0], lines[1]
+
+
+def patternCount(text, pattern):
+    count = 0
+    end = len(text) - len(pattern)
+    patternLength = len(pattern)
+    for i in range(end):
+        if text[i:i+patternLength] == pattern:
+            count+=1
+    return count
+
+
+text, pattern = readInput('dataset_2_6.txt')
+print(patternCount(text,pattern))
